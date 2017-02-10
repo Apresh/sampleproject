387bbe8b814f2a7f0b1c0403bbf11afc9ecd6fc9
arokalla <arokalla@AppShark89.APPSHARK.IN>
2017-02-10 12:26:57 +0530

    Inital Commit

---
+++ src/SampleJava.java
@@ -0,0 +1,15 @@
+/**
+* The HelloWorld program implements an application that
+* simply displays "Hello World!" to the standard output.
+*
+* @author  Apresh
+* @version 1.0
+* @since   2017-02-10 
+*/
+public class SampleJava {
+
+   public static void main(String[] args) {
+      /* Prints Hello, World! on standard output.*/
+      System.out.println("Hello World!");
+   }
+}
