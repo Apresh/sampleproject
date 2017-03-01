4d287698b8f31dfc13b07995c3ae7b9281f36133
arokalla <arokalla@AppShark89.APPSHARK.IN>
2017-03-01 12:12:13 +0530

    Line 1

--- src/SampleJava.java
+++ src/SampleJava.java
@@ -11,6 +11,6 @@ public class SampleJava {
    public static void main(String[] args) {
       /* Prints Hello, World! on standard output.*/
       System.out.println("Hello World!");
-      System.out.println("Hello World!");
+      System.out.println("Line 1");
    }
 }
